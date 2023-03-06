25fa1c8089f02a2e22a392c0810402f14bfd7281
Vincent Arif Kurniawan <vincent.arifk@student.uns.ac.id>
2023-03-06 10:08:20 +0700

    commit pertama

---
+++ Vincent6Maret.py
@@ -0,0 +1,15 @@
+nama_depan = "Vincent"
+nama_belakang = "Kurniawan"
+
+angka = 18
+desimal = 0.75
+boolean = True
+kelas_D = ["Vincent", "Kelas D", "2022/2023"]
+data = {"nama": "Vincent AK", "jurusan": "Teknik Industri", "kelas": "D"}
+
+print(nama_depan)
+print("A.")
+print(nama_belakang)
+print("Nama saya adalah Vincent")
+
+print(f"nama saya adalah {nama_depan} {nama_belakang} dari kelas {data['kelas']}")
