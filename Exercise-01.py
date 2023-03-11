22e197f79634cdb06866488657e6bf457660d7ab
Vincent Arif K <vincent.arifk@student.uns.ac.id>
2023-03-11 11:01:17 +0700

    Commit Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,19 @@
+#Exercise-01
+Nama = "Vincent Arif Kurniawan"
+NIM = "I0322127"
+TempatLahir = "Surakarta"
+TanggalLahir = "28 Juni 2004"
+TempatTinggal = "Karanganyar"
+Cerita1 = "Saya tinggal bersama orang tua dan kakak saya. Suasana rumah cukup sepi, dengan gerbang rumah menghadap hutan kecil. Sinyal internet cukup baik, namun sering terjadi kendala jika perlu melakukan kelas daring melalui Zoom."
+Cerita2 = "Meski saya tinggal di Karanganyar, tempat tinggal saya sangat dekat dengan Surakarta, dan aktivitas keseharian saya selalu dilakukan di Surakarta, seperti kuliah dan bermain musik. Surakarta adalah kota yang padat penduduk, sehingga cukup sering terjadi macet di beberapa titik. Pembangunan di kota Surakarta berjalan dengan cukup pesat, dibuktikan dengan beberapa hal seperti pemugaran Pura Mangkunegaran, pembangunan Masjid Raya Sheikh Zayed Solo, dan sebagainya."
+Harapan = "Saya mampu menerapkan segala hal yang saya pelajari selama berkuliah di Teknik Industri UNS dalam kehidupan sehari-hari, termasuk dalam dunia kerja sebagai target jangka panjang, mulai dari ilmu, keterampilan, dan sikap. Saya bercita-cita untuk bekerja di bidang musik yang saya minati dengan menerapkan ilmu, keterampilan, dan sikap yang saya pelajari, dalam bidang kontrol kualitas (QC) produksi alat musik."
+
+print("===========================================")
+print("Nama saya adalah ", Nama)
+print("Nomor Induk Mahasiswa saya adalah ", NIM)
+print("Saya lahir di ", TempatLahir, "tepatnya pada tanggal ", TanggalLahir)
+print("Saya bertempat tinggal di ", TempatTinggal, ". ", Cerita1)
+print(Cerita2)
+print("Harapan saya sebagai lulusan Teknik Industri adalah ", Harapan)
+print("Terima Kasih")
+print("===========================================")
